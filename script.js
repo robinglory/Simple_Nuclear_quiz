6d542b6ca26b4d48e4e973ff8f92decdc014caea
Robin Glory <80534929+robinglory@users.noreply.github.com>
2023-06-28 23:29:56 +0630

    Update script.js

--- script.js
+++ script.js
@@ -113,7 +113,7 @@
 			questionElement.innerHTML = `You scored ${score} out of ${questions.length}! You can't build Nuclear Plant in Myanmar as your parameters did not meet with the desired outcome.`;
 
 		} else{
-			questionElement.innerHTML = `Congratulation!! You can build a Nuclear Power Plant in this place <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d235209.25236681502!2d96.28660224055365!3d22.908037957918975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x373360f8693e81fb%3A0x985888ad677fd9b3!2sMogoke!5e0!3m2!1smy!2smm!4v1687967465080!5m2!1smy!2smm" width="80%" height="60%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+			questionElement.innerHTML = `Congratulation!! You can build a Nuclear Power Plant in this place <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d235209.25236681502!2d96.28660224055365!3d22.908037957918975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x373360f8693e81fb%3A0x985888ad677fd9b3!2sMogoke!5e0!3m2!1smy!2smm!4v1687967465080!5m2!1smy!2smm" width="50%" height="50%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
 
 		}
 		nextButton.innerHTML="Play Again";

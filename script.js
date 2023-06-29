0a0b15f124aa13d9d9367c763f8d53e777e39c7e
Robin Glory <80534929+robinglory@users.noreply.github.com>
2023-06-29 09:17:37 +0630

    Update script.js
    Updating

--- script.js
+++ script.js
@@ -110,7 +110,7 @@
 	function showScore(){
 		resetState();
 		if (score<5) {
-			questionElement.innerHTML = `You scored ${score} out of ${questions.length}! You can't build Nuclear Plant in Myanmar as your parameters did not meet with the desired outcome.`;
+			questionElement.innerHTML = `You scored ${score} out of ${questions.length}! You can't build Nuclear Plant in Myanmar as your parameters did not meet with the desired outcomes.`;
 
 		} else{
 			questionElement.innerHTML = `Congratulation!! You can build a Nuclear Power Plant in this place <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14703.189323060024!2d95.9680868020659!3d22.88394396833569!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3734c1f7982dbabd%3A0xe191f3e5f078dccb!2z4YCe4YCV4YCt4YCQ4YC64YCA4YC74YCJ4YC64YC4!5e0!3m2!1smy!2smm!4v1688005355220!5m2!1smy!2smm" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`

f746cc0782e71a1d7d6e362f0e4177d3954df840
Robin Glory <80534929+robinglory@users.noreply.github.com>
2023-06-29 08:54:48 +0630

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
 	{
 		question: "Type of environment should be established for a Nuclear Power Plant",
 		answers: [ 
-				{text: "Located some body of water", correct: true},
+				{text: "In the vicinity of a river, sea, or lake.", correct: true},
 				{text: "On the mountain" , correct: false},
 				{text: "On the dessert", correct: false},
 				{text: "Inside the hinderland", correct: false},
@@ -23,13 +23,13 @@
 				{text: "Yangon", correct: false},
 				{text: "Mandalay" , correct: false},
 				{text: "Tanuggyi", correct: false},
-				{text: "Mogoke", correct: true},
+				{text: "Mogoke(Thabeikkyin Township)", correct: true},
 			]
 	},
 	{
 		question: "Land area requirement",
 		answers: [
-				{text: "less than 1 square mile", correct: false},
+				{text: "Less than 1 square mile", correct: false},
 				{text: "1 square mile" , correct: false},
 				{text: "A little more than 1 square mile", correct: true},
 				{text: "2 square mile", correct: false},
@@ -113,7 +113,7 @@
 			questionElement.innerHTML = `You scored ${score} out of ${questions.length}! You can't build Nuclear Plant in Myanmar as your parameters did not meet with the desired outcome.`;
 
 		} else{
-			questionElement.innerHTML = `Congratulation!! You can build a Nuclear Power Plant in this place <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d235209.25236681502!2d96.28660224055365!3d22.908037957918975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x373360f8693e81fb%3A0x985888ad677fd9b3!2sMogoke!5e0!3m2!1smy!2smm!4v1687967465080!5m2!1smy!2smm" width="400" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+			questionElement.innerHTML = `Congratulation!! You can build a Nuclear Power Plant in this place <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14703.189323060024!2d95.9680868020659!3d22.88394396833569!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3734c1f7982dbabd%3A0xe191f3e5f078dccb!2z4YCe4YCV4YCt4YCQ4YC64YCA4YC74YCJ4YC64YC4!5e0!3m2!1smy!2smm!4v1688005355220!5m2!1smy!2smm" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
 
 		}
 		nextButton.innerHTML="Play Again";
